b221b48b8c0ae01e8b48c44c12a2d7a8ebc9d7a7
Leo Deng <leo7deng@gmail.com>
2025-02-06 23:11:28 -0500

    Update GitHub Actions config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? '/chatapp' : '',
+  basePath: isProd ? '/chatapp' : '',
+  output: 'export'
 };
 
 export default nextConfig;
