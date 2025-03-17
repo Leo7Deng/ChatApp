ceed8db7af61c2e4ef4e222f0f3c068fea834f8a
Leo Deng <leo7deng@gmail.com>
2025-03-17 10:57:09 -0700

    Add search modal to dashboard

--- frontend/app/components/Dashboard.tsx
+++ frontend/app/components/Dashboard.tsx
@@ -5,6 +5,7 @@ import React, { useEffect, useRef, useState } from "react";
 import CreateCircleModal from "./CreateCircleModal";
 import EditModal from "./EditModal";
 import InviteModal from "./InviteModal";
+import SearchModal from "./SearchModal";
 import { useFetchDashboard } from "../hooks/useFetchDashboard";
 import { useWebSocketDashboard } from "../hooks/useWebsocketDashboard";
 import { Circle, Message, WebSocketMessage } from "../types";
@@ -45,6 +46,17 @@ export default function Dashboard2() {
     const handleClose = (event: HandleCloseEvent) => {
         if (event.target.classList.contains('modal-container')) {
             setOpenModal(false);
+        }
+    };
+
+    // Search chat
+    const [openSearchModal, setOpenSearchModal] = useState(false);
+    function handleOpenSearchModal() {
+        setOpenSearchModal(true);
+    }
+    const handleSearchClose = (event: HandleCloseEvent) => {
+        if (event.target.classList.contains('modal-container')) {
+            setOpenSearchModal(false);
         }
     };
 
@@ -249,11 +261,30 @@ export default function Dashboard2() {
                                 <EditModal isOpen={openEditModal} setOpen={() => setOpenEditModal(false)} circleId={selectedCircleID} />
                             </div>
                         </div>}
+                        {openSearchModal && <div className="modal-container" onClick={handleSearchClose}>
+                            <div className="modal inset-0 bg-black bg-opacity-50 z-50">
+                                <SearchModal isOpen={openSearchModal} setOpen={() => setOpenSearchModal(false)} circleId={selectedCircleID} />
+                            </div>
+                        </div>}
                     </div>
                     <div className="chat-container">
                         <div className="chat-title">
                             <h1>{circles.find((circle) => circle.id === selectedCircleID)?.name}&nbsp;</h1>
                             <div className="chat-menu">
+                                <button
+                                    className="button-size group disabled:opacity-50 disabled:cursor-not-allowed"
+                                    onClick={selectedCircleID !== "" ? handleOpenSearchModal : undefined}
+                                    disabled={selectedCircleID === ""}
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-5 opacity-75" viewBox="0 0 30 30" strokeWidth="1" stroke="currentColor" fill="currentColor">
+                                        <path d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z"></path>
+                                    </svg>
+                                    <span
+                                        className="invisible absolute start-1/2 top-full mt-5 -translate-x-1/2 rounded bg-gray-900 px-1.5 py-1.5 text-xs font-medium text-white group-hover:visible"
+                                    >
+                                        Search Chat
+                                    </span>
+                                </button>
                                 <button
                                     className="button-size group disabled:opacity-50 disabled:cursor-not-allowed"
                                     onClick={selectedCircleID !== "" ? handleOpenEditModal : undefined}
